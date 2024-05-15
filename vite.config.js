96bbec8a385d9749c89878714d9b7e292498e48d
Krish <111554369+krishanprajapat23@users.noreply.github.com>
2024-05-15 13:39:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vocab-voyage/',
+  base: '/vocab-voyage-react/',
   resolve: {
     alias: {
       // Add an alias for Bootstrap to resolve the `~` prefix
